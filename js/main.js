0256a2060cc5b1316577411f675893c811faf3f9
Piotr Materna <piotr.materna0@gmail.com>
2022-05-16 20:07:09 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const burgerBtn = document.querySelector('.hamburger')
+const navMobile = document.querySelector('.nav__mobile')
+const navItem = document.querySelectorAll('.nav__item')
+const footerYear = document.querySelector('.footer__year')
+
+const handleNav = () => {
+	burgerBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav__mobile--active')
+
+	handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	navItem.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+burgerBtn.addEventListener('click', handleNav)
+handleCurrentYear()
