3036f78fe7776109fe886684afbfebe61f74a4b8
Piotr Materna <piotr.materna0@gmail.com>
2022-05-26 23:42:07 +0200

    desktop gallery fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 const { src, dest } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const cssnano = require('gulp-cssnano')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
 
 function sassCompiler(done) {
-	src('./src/sass/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(dest('./dist/css'))
+	src('./src/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		// .pipe(sourcemaps.init())
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(dest('./dist/css'))
 	done()
 }
 
 exports.sassCompiler = sassCompiler
+
+// gulp.task('default', function () {
+//     return gulp.
+// });
